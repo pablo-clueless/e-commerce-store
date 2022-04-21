d11411898465a923501d013841f8cf834af51743
Olawale O Samson <smsnmicheal@gmail.com>
2022-04-21 19:15:40 +0100

    chnaged title font

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -16,6 +16,7 @@ export default makeStyles((theme) => ({
     alignItems: 'center',
     display: 'flex',
     textDecoration: 'none',
+    fontFamily: 'Montserrat Alternates'
   },
   image: {
     marginRight: '10px',
